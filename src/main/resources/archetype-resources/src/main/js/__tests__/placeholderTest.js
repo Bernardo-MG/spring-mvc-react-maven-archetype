25feac1dac45f46fc1a54f2393370818d1982c08
Bernardo <programming@wandrell.com>
2018-08-15 22:46:26 +0200

    Added JS tests

---
+++ src/main/resources/archetype-resources/src/main/js/__tests__/placeholderTest.js
@@ -0,0 +1,12 @@
+import model from 'players/reducers';
+import * as types from 'players/actions/actionTypes';
+
+describe('Placeholder test', () => {
+   it('does nothing', () => {
+      expect(
+            true
+      ).toEqual(
+            true
+      )
+   })
+});
