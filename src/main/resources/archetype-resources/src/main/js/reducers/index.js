76b8268bcc94f0dcbb46c932e445f28ee3923341
Bernardo <programming@wandrell.com>
2018-08-17 22:02:23 +0200

    Removed unused imports

--- src/main/resources/archetype-resources/src/main/js/reducers/index.js
+++ src/main/resources/archetype-resources/src/main/js/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import views from 'views/reducers';
-import { players, ratedPlayers } from 'players/reducers/pagination';
 
 const dreadballApp = combineReducers({
    routing,
