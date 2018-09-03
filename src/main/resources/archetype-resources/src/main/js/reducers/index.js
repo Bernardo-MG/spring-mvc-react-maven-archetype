6bdbf8fdef44bb2ab927955dc48722b8fdff2f3b
Bernardo <programming@wandrell.com>
2018-09-04 00:01:19 +0200

    Fixed import

--- src/main/resources/archetype-resources/src/main/js/reducers/index.js
+++ src/main/resources/archetype-resources/src/main/js/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import views from 'views/reducers';
-import entities from 'example/reducers/pagination';
+import { entities } from 'example/reducers/pagination';
 
 const pagination = combineReducers({
    entities
