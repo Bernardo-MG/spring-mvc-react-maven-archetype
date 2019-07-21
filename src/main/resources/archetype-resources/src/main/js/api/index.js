4c95ab2494cfb5d0b8b4d80401e1f5a6b9353751
Bernardo <programming@wandrell.com>
2019-07-21 21:45:22 +0200

    Corrected URL

--- src/main/resources/archetype-resources/src/main/js/api/index.js
+++ src/main/resources/archetype-resources/src/main/js/api/index.js
@@ -1,6 +1,6 @@
 import superagent from 'superagent';
 
-const API_ROOT = 'http://localhost';
+const API_ROOT = 'http://localhost:8080';
 
 const requests = {
    get: (url) => superagent.get(`${API_ROOT}${url}`).then((response) => JSON.parse(response.text))
