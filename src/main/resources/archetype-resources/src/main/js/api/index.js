ada60bf2a19c6dfbd623cb0d179166041de16c03
Bernardo <programming@wandrell.com>
2019-08-11 18:43:49 +0200

    Corrected creation command

--- src/main/resources/archetype-resources/src/main/js/api/index.js
+++ src/main/resources/archetype-resources/src/main/js/api/index.js
@@ -4,11 +4,11 @@ const API_ROOT = 'http://localhost:8080';
 
 const requests = {
    get: (url) => superagent.get(`${API_ROOT}${url}`).then((response) => JSON.parse(response.text)),
-   put: (url, body) => superagent.put(`${API_ROOT}${url}`, body).then((response) => JSON.parse(response.text))
+   post: (url, body) => superagent.post(`${API_ROOT}${url}`, body).then((response) => JSON.parse(response.text))
 };
 
 const Entities = {
-   create: (name) => requests.put('/rest/entity', { name }),
+   create: (name) => requests.post('/rest/entity', { name }),
    byTitle: (query, page, perPage) => requests.get(`/rest/entity?query=${query}&&page=${page}&&size=${perPage}`)
 };
 
