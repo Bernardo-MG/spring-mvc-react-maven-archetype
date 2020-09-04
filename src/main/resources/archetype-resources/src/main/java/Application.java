cf66a34ec35edb2ed3bc92be8ad89d86bfaada7b
Bernardo <programming@bernardomg.com>
2020-09-05 01:28:25 +0200

    Added executable class

---
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,18 @@
+
+package com.bernardomg.jqassistant.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    public Application() {
+        super();
+    }
+
+}
