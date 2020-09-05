4c9fcd322a7917f7240011b973b596b0c40d475f
Bernardo <programming@bernardomg.com>
2020-09-05 14:53:11 +0200

    Corrected package

--- src/main/resources/archetype-resources/src/main/java/Application.java
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,5 +1,5 @@
 
-package com.bernardomg.jqassistant.test;
+package ${package};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
