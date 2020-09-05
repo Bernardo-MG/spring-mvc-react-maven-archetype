5ba7771aac54937750f10f413bd392aaec8724f7
Bernardo <programming@bernardomg.com>
2020-09-05 15:38:30 +0200

    Removed unused dependency

--- src/main/resources/archetype-resources/src/main/java/controller/GlobalExceptionHandler.java
+++ src/main/resources/archetype-resources/src/main/java/controller/GlobalExceptionHandler.java
@@ -35,7 +35,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import ${package}.controller.GlobalExceptionHandler;
