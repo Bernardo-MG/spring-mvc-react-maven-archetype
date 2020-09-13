f2c9b8424e7a130fdae5e6a0d71d88ac22fee1e5
Bernardo <programming@bernardomg.com>
2020-09-13 15:55:46 +0200

    Registered notification components

--- src/main/resources/archetype-resources/src/main/js/reducers/index.js
+++ src/main/resources/archetype-resources/src/main/js/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import entity from 'entities/reducers';
 import search from 'search/reducers';
+import notification from 'notify/reducers';
 
 const rootReducer = combineReducers({
    entity,
-   search
+   search,
+   notification
 });
 
 export default rootReducer;
