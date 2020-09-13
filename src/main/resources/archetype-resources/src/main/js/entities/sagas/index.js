ab851100346e6cb43f9fc6a7143e9d2b4c7d9115
Bernardo <programming@bernardomg.com>
2020-09-13 16:43:51 +0200

    Fixed import

--- src/main/resources/archetype-resources/src/main/js/entities/sagas/index.js
+++ src/main/resources/archetype-resources/src/main/js/entities/sagas/index.js
@@ -1,12 +1,12 @@
 import { put, takeLatest, call } from 'redux-saga/effects';
 import { CREATE_ENTITY } from 'entities/actions/types';
 import { createSuccess as success, createFailure as failure } from 'entities/actions';
-import api from 'api';
+import { Entities } from 'api/requests';
 
 export function* create(action) {
    let response;
    try {
-      response = yield call(api.Entities.create, action.payload);
+      response = yield call(Entities.create, action.payload);
       yield put(success(response));
    } catch (err) {
       yield put(failure(err));
